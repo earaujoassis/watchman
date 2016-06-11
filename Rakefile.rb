96759f27e7d377ebc2c5ee0a759538a0074f5aa2
Ewerton Assis <earaujoassis@gmail.com>
2016-06-11 17:36:57 -0300

    Add URI health check (response time) and tests

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,3 +4,10 @@ desc 'Open an IRB console with the Watchman module available'
 task :console do
   sh %{irb -r ./console}
 end
+
+desc 'Run the specs for the sequel-seed'
+task :test do
+  sh %{#{FileUtils::RUBY} -S bundle exec rspec}
+end
+
+task default: [:test]
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'bundler/setup'
+Bundler.setup(:default, :development, :test)
+
+require File.expand_path(File.dirname(__FILE__) + '../../lib/watchman')
+
+RSpec.configure do |config|
+end
