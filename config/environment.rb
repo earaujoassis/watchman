b48c1fad1e079224b28d0c3ad66a7eee8fadf980
Ewerton Assis <earaujoassis@gmail.com>
2019-05-29 00:16:44 -0300

    feature: update Hanami version

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,47 @@
 require 'bundler/setup'
 require 'hanami/setup'
+#require 'hanami/model'
 require_relative '../lib/backdoor'
 require_relative '../apps/web/application'
 
-Hanami::Container.configure do
+Hanami.configure do
   mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'postgresql://localhost/backdoor_development'
+    #
+    adapter :sql, ENV.fetch('DATABASE_URL')
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/backdoor/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: http://hanamirb.org/guides/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json, filter: []
+
+    mailer do
+      delivery :smtp, address: ENV.fetch('SMTP_HOST'), port: ENV.fetch('SMTP_PORT')
+    end
+  end
 end
--- lib/backdoor.rb
+++ lib/backdoor.rb
@@ -1,13 +1,2 @@
-require 'hanami/mailer'
-Dir["#{ __dir__ }/backdoor/**/*.rb"].each { |file| require_relative file }
-
-Hanami::Mailer.configure do
-  root "#{ __dir__ }/backdoor/mailers"
-
-  # See http://hanamirb.org/guides/mailers/delivery
-  delivery do
-    development :test
-    test        :test
-    # production :smtp, address: ENV['SMTP_PORT'], port: 1025
-  end
-end.load!
+module Backdoor
+end
