499e53612dbfc0a43095aaa3853e821f3b6bc200
Ewerton Assis <earaujoassis@gmail.com>
2019-06-04 00:57:21 -0300

    feature: add project page dynamic content

---
+++ lib/backdoor/services/github.rb
@@ -0,0 +1,11 @@
+require 'octokit'
+
+class Backdoor::Services::GitHub
+  def initialize(access_token)
+    @client = Octokit::Client.new(access_token: access_token, per_page: 50)
+  end
+
+  def repos
+    @client.repos({}, query: {type: 'owner', sort: 'asc'})
+  end
+end
---
+++ web/src/components/UI/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.css';
+
+const SpinningSquare = () => (
+  <div className="UISpinningSquare-wrapper">
+    <p>Loading</p>
+    <div className="UISpinningSquare" />
+  </div>
+);
+
+export {
+  SpinningSquare
+};
