1fc058e4757e79656640efdc17567643c2befe84
Ewerton Assis <earaujoassis@gmail.com>
2019-07-03 03:14:44 -0300

    Prune Docker system when re-deploying application

--- bin/redeploy.sh
+++ bin/redeploy.sh
@@ -4,3 +4,6 @@ set -ex
 
 docker-compose build watchman
 docker-compose up --no-deps -d watchman
+# docker rmi $(docker images -a --filter=dangling=true -q)
+# docker rm $(docker ps --filter=status=exited --filter=status=created -q)
+docker system prune -a -f
