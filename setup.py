0038fe93dbaa9bc1786dd9719cf6ad2ed0c21809
Ewerton Assis <earaujoassis@gmail.com>
2019-07-04 22:32:34 -0300

    hotfix: fix pip/project name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="watchamn",
+    name="watchman",
     version="0.1.7",
     license="MIT",
     author="Ewerton Carlos Assis",
