8b62a2b9791efe7ed682de9cd3ed5f204c1b865f
Carlos A. <588595+earaujoassis@general.noreply.quatrolabs.com>
2019-08-27 17:02:31 -0300

    feature: add tests for the server-report features

--- spec/api/controllers/server/report_create_spec.rb
+++ spec/api/controllers/server/report_create_spec.rb
@@ -1,10 +1,56 @@
 RSpec.describe Api::Controllers::Server::ReportCreate, type: :action do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:params) do
+    Hash[
+      server: {
+        hostname: "testing.example.com",
+        ip: "1.1.1.1",
+        latest_version: "0.0.0"
+      },
+      report: {
+        subject: "Testing report"
+      }
+    ]
+  end
+
+  before(:each) do
+    ReportRepository.new.clear
+    ServerRepository.new.clear
+  end
 
   it "should return Bad Request when there's any missing attribute" do
     response = action.call(Hash.new)
     status_code = response[0]
     expect(status_code).to eq 400
+  end
+
+  it "should create a server and a report for it" do
+    response = action.call(params)
+    status_code = response[0]
+    expect(status_code).to eq 201
+    body = JSON.parse(response[2].first)
+    expect(body["report"]["id"]).not_to be_empty
+  end
+
+  it "should create a report for an existing server" do
+    server = ServerRepository.new.create({
+      hostname: "testing.example.com",
+      ip: "1.1.1.1",
+      latest_version: "0.0.0"
+    })
+    response = action.call(Hash[
+      server: {
+        hostname: "testing.example.com",
+        ip: "1.1.1.1",
+        latest_version: "0.1.0"
+      },
+      report: {
+        subject: "Testing report"
+      }
+    ])
+    status_code = response[0]
+    expect(status_code).to eq 201
+    body = JSON.parse(response[2].first)
+    expect(body["report"]["id"]).not_to be_empty
   end
 end
