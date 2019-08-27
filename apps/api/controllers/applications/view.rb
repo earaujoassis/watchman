87d60f9a35891db81f776db607670d43e2564ef3
Carlos A. <588595+earaujoassis@general.noreply.quatrolabs.com>
2019-08-27 00:27:40 -0300

    feature: improve tests and fix issues

--- apps/api/controllers/applications/view.rb
+++ apps/api/controllers/applications/view.rb
@@ -10,6 +10,7 @@ module Api
         def call(params)
           repository = ApplicationRepository.new
           application = repository.find(params[:id])
+          halt 404, { error: 'unknown application' } if application.nil?
           self.body = application.configuration_file
         end
       end
