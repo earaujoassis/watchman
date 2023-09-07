47a6c0738e13a9a964adf0f0d219ba2db559fccf
Carlos A <593456+earaujoassis@users.noreply.github.com>
2023-09-07 12:44:30 -0300

    hotfix: sanitize quotes in yaml charts (#66)

--- lib/executor/helpers/chart_helpers.rb
+++ lib/executor/helpers/chart_helpers.rb
@@ -13,13 +13,13 @@ module Executor
       def update_image_tag(tag:, path: ["image", "tag"])
         content = @content.clone
         *head, tail = path
-        head.inject(content) { |hash, key| hash[key] }[tail] = "'#{tag}'"
+        head.inject(content) { |hash, key| hash[key] }[tail] = "'sanitizeQuote: #{tag}'"
         @content = content
       end
 
       def save_values
         File.open(@path, "w") do |f|
-          f.puts @content.to_yaml.to_s.gsub("---\n", "")
+          f.puts @content.to_yaml.to_s.gsub("---\n", "").gsub("sanitizeQuote: ", "")
         end
         true
       end
