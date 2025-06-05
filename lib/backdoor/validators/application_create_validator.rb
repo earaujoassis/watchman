31df66da8895ae45e3fd2da85db8f3ae90f10499
Carlos A. <588595+earaujoassis@general.noreply.quatrolabs.com>
2025-06-05 13:28:23 -0300

    deprecation: final line for this tree

--- lib/backdoor/validators/application_create_validator.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationCreateValidator < Hanami::Validator
-  json do
-    required(:full_name).value(Types::StrippedString)
-    optional(:description).value(Types::StrippedString)
-    required(:managed_realm).value(Types::StrippedString)
-    optional(:managed_projects).value(Types::StrippedString)
-  end
-end
